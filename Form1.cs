b51074bc54d07b8e304f8caefea2351393075919
Yasindu <yasindumalmith058@gmail.com>
2025-06-16 17:41:20 +0530

    edit

--- Form1.cs
+++ Form1.cs
@@ -19,7 +19,7 @@ namespace LibraryManagementSystem
             user us = new user(userName, userPW);
             string role = us.checkUser();
             bool checkUserNamePw = us.ValidateUser(userName, userPW);
-            if (role == "admi")
+            if (role == "adming")
             {
                 this.Hide();
                 Form2 dashboard = new Form2();
